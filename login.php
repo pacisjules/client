85344745ea08257168a20ad5a4d914fa7acf0ca7
Ishimwe Jules Pacis (Mr Robot) <51479761+pacisjules@users.noreply.github.com>
2024-03-30 19:19:02 +0200

    Update login.php

--- login.php
+++ login.php
@@ -28,11 +28,11 @@ $user_id = $_SESSION['user_id'];
 <body class="bg-gradient-primary">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-9 col-lg-12 col-xl-10">
+            <div class="col-md-9 col-lg-12 col-xl-10" style="margin-top: 40px;">
                 <div class="card shadow-lg o-hidden border-0 my-5">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-flex">
+                    <div class="card-body p-0" >
+                        <div class="row" >
+                            <div class="col-lg-6 d-none d-lg-flex" >
                                 <div class="flex-grow-1 bg-login-image" style="background-image: url(&quot;assets/img/easter.jpg&quot;);"></div>
                             </div>
                             <div class="col-lg-6">
