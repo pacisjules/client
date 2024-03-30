5916a173ac87a5836447523fce9079dfce7a1b72
Ishimwe Jules Pacis (Mr Robot) <51479761+pacisjules@users.noreply.github.com>
2024-03-30 19:13:31 +0200

    NEW UPDATES

--- login.php
+++ login.php
@@ -33,7 +33,7 @@ $user_id = $_SESSION['user_id'];
                     <div class="card-body p-0">
                         <div class="row">
                             <div class="col-lg-6 d-none d-lg-flex">
-                                <div class="flex-grow-1 bg-login-image" style="background-image: url(&quot;assets/img/stock.jpg&quot;);"></div>
+                                <div class="flex-grow-1 bg-login-image" style="background-image: url(&quot;assets/img/easter.jpg&quot;);"></div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
