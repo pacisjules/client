5caf3a1d4ef9bfeccb505eb9508e22e82ef945a9
Esdras Ntwari <103323625+ezran2022@users.noreply.github.com>
2024-04-04 22:51:33 +0200

    C

--- functions/debts/getalldebtscompanyspt.php
+++ functions/debts/getalldebtscompanyspt.php
@@ -55,7 +55,7 @@ while ($row = $result->fetch_assoc()) {
         <td>' . $row['phone'] . '</td>
         <td>' . $row['address'] . '</td>
         <td> FRW ' . $formattedTotalAmount . '</td>
-        <td style="color:'.$endis.'; font-weight:bold;">' . $sts . '</td>
+        
         <td>' . $row['due_date'] . '</td>
         <td class="d-flex flex-row justify-content-start align-items-center">
             <a class="nav-link active" href="debtdetails.php?customer_id=' . $myid . '">
