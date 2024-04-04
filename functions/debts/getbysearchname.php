9aec5b60c4d7d502f4e89ef3b2494dcad29c70ae
Esdras Ntwari <103323625+ezran2022@users.noreply.github.com>
2024-04-05 00:53:33 +0200

    cv

--- functions/debts/getbysearchname.php
+++ functions/debts/getbysearchname.php
@@ -26,13 +26,50 @@ $sql = "SELECT DISTINCT
         WHERE 
             DE.sales_point_id = $spt AND CU.names LIKE '%$names%'";
 
+$value = "";
 $result = mysqli_query($conn, $sql);
 
-$value = "";
 $num = 0;
 
+// Convert the results to an array of objects
+$comp = array();
 while ($row = $result->fetch_assoc()) {
-    // Process your rows here
+    $myid = $row['customer_id'];
+    $num += 1;
+
+    $sts = "";
+    $endis = "";
+    $icon = "";
+
+    if ($row['status'] == 1) {
+        $sts = "Loan";
+        $endis = "red";
+        $icon = "bi bi-x-circle";
+    } else {
+        $sts = "Full Paid";
+        $endis = "green";
+        $icon = "fa fa-check-square text-white";
+    }
+    
+     $formattedTotalAmount = number_format($row['Amount']); 
+
+    $value .= '
+        <tr>
+        <td>' . $num . '. ' . $row['names'] . '</td>
+        <td>' . $row['phone'] . '</td>
+        <td>' . $row['address'] . '</td>
+        <td> FRW ' . $formattedTotalAmount . '</td>
+        
+        <td>' . $row['due_date'] . '</td>
+        <td class="d-flex flex-row justify-content-start align-items-center">
+            <a class="nav-link active" href="debtdetails.php?customer_id=' . $myid . '">
+                <button class="btn btn-success"  rounded-circle" style="background-color:#040536; border-radius:15px;" type="button">
+                    <i class="fas fa-eye" style="font-size:20px; color: white; margin-top:3px;"></i>
+                </button>
+            </a>
+        </td>
+        </tr>
+    ';
 }
 
 $response = array(
