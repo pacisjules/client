3dbe257865d2a095abfcedf4332b4a534d3db2d7
Esdras Ntwari <103323625+ezran2022@users.noreply.github.com>
2024-05-19 17:44:23 +0200

    c

--- functions/connection.php
+++ functions/connection.php
@@ -4,17 +4,17 @@
 //$servername = "86.38.202.52";
 
 
-// $servername = "localhost";
-// $username = "u774778522_sell_user_db";
-// $password = "Ishimuko@123";
-// $dbname = "u774778522_selleasep_db";
+$servername = "localhost";
+$username = "u774778522_sell_user_db";
+$password = "Ishimuko@123";
+$dbname = "u774778522_selleasep_db";
 
 
 //localhost Connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "newdb";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "newdb";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
