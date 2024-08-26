16dcc7d221b9ca2d06cacd1e4087605da29eb2e1
Ishimwe Jules Pacis (Mr Robot) <51479761+pacisjules@users.noreply.github.com>
2024-08-26 19:18:50 +0200

    Update sales-panelfixed.php

--- sales-panelfixed.php
+++ sales-panelfixed.php
@@ -126,7 +126,7 @@ include('getuser.php');
         <p>Home</p></a>
        </div>
        <div class="clbtns">
-       <a href="products.php"><img src="styles/icons/features.png" alt="" srcset="">
+       <a href="product.php"><img src="styles/icons/features.png" alt="" srcset="">
         <p>Products</p></a>
        </div>
        <!-- <div class="clbtns">
